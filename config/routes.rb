1529129fe7e049bd9f408a5964b34a68d28d42ba
tom lopez <tomlpz.pro2@gmail.com>
2026-05-19 13:40:47 +0200

    Add tasks edit and update feature

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,11 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "/tasks", to: "tasks#index"
+  get "/tasks/new", to: "tasks#new", as: :new_task
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update"
   get "tasks/:id", to: "tasks#show", as: :task
-
+  post "tasks", to: "tasks#create"
   # Defines the root path route ("/")
   # root "posts#index"
 end
