48110ec19d45bafc80754dd31198dfb7183177c9
tom lopez <tomlpz.pro2@gmail.com>
2026-05-19 12:23:54 +0200

    Add tasks show page and link from index

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "/tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
 
   # Defines the root path route ("/")
   # root "posts#index"
